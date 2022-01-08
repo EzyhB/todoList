c64aebd033c36b05fb2f5becfa5ebf0d89323b4f
Costa Vorontsov <me.costav@gmail.com>
2022-01-08 19:15:56 +0000

    WIP

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/Components/App/pp.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
@@ -15,11 +15,9 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    {process.env.REACT_APP_NAME}
                 </a>
             </header>
         </div>
     );
 }
-
-export default App;
